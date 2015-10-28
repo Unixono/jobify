138664f7f96ceec6a88dabae669664c51d1e2894
Leandro Balmaceda <balmacedalm@gmail.com>
2015-10-28 18:31:56 -0300

    Add offer list screen

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+// Create an express instance.
 var express = require('express');
 var app = express();
 
 // Set the path to the website files.
 app.use(express.static(__dirname + '/public/dist'));
+
+// Add this to allow CORS.
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:9000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 var port = 3000;
 app.listen(port);
