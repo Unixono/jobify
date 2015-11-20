9a9dcd04845d38db958e0e35252cc4c74fd9cb0a
carneroalejandro <carneroalejandro@gmail.com>
2015-11-20 19:27:13 -0300

    #5199 wip, and some extra changes

--- public/app/scripts/controllers/register.js
+++ public/app/scripts/controllers/register.js
@@ -8,11 +8,12 @@
  * Controller of the publicApp
  */
 angular.module('publicApp')
-.controller('RegisterCtrl', function ($scope, $location) {
+.controller('RegisterCtrl', function ($scope, $window, $location) {
 
-  $scope.role = developer;
+  $scope.role = 'developer';
 
   $scope.passwordHasSpaces = false;
+  $scope.showLoading = false;
   var userEmail;
 
 
@@ -23,7 +24,7 @@ angular.module('publicApp')
     // $scope.passwordComplexity = score;
     if( password.indexOf(" ") > -1 ) {
       $scope.passwordHasSpaces = true;
-      $scope.passwordAdvice = $rootScope.translate('wa_spaces_not_allowe');
+      $scope.passwordAdvice = 'Spaces not allowed';
     }
     else {
       $scope.passwordHasSpaces = false;
@@ -31,7 +32,7 @@ angular.module('publicApp')
     }
 
     if(password !== retypePassword) {
-      $scope.retypePasswordAdvice = $rootScope.translate('wa_doesnt_match');
+      $scope.retypePasswordAdvice = 'Password doesnt match';
     }
     else {
       $scope.retypePasswordAdvice = "";
@@ -55,7 +56,7 @@ angular.module('publicApp')
   $scope.retypePasswordAdvice = "";
   $scope.retypePasswordChanged = function(password, retypePassword) {
     if(password !== retypePassword) {
-      $scope.retypePasswordAdvice = $rootScope.translate('wa_doesnt_match');
+      $scope.retypePasswordAdvice = 'Password doesnt match';
     }
     else {
       $scope.retypePasswordAdvice = "";
@@ -74,26 +75,24 @@ angular.module('publicApp')
 
     userEmail = email;
 
-    appletInteraction._signupUser(email, password, cpassword, name, 'signUpCallback');
+    // appletInteraction._signupUser(email, password, cpassword, name, 'signUpCallback');
   };
 
   $window.signUpCallback = function(returnValue) {
     // console.log('signUpCallback');
-    var response = angular.fromJson(returnValue);
-    $scope.showLoading = false;
-    var error = errorHandler.checkCriticalError(response);
-    if(error === false) {
-      if( response.severity !== null ) {
+    // var response = angular.fromJson(returnValue);
+    // $scope.showLoading = false;
+    // var error = errorHandler.checkCriticalError(response);
+    // if(error === false) {
+      // if( response.severity !== null ) {
         // If there was an error, print the error message.
-        $scope.errorText = $rootScope.translateError(response.errorCode);
-      }
-      else {
-        userAccount.setUserEmail( userEmail );
-        var nextView = { view:'ALMOST_DONE' };
-        viewManager.setNextView( nextView );
-      }
-    }
-    $scope.$apply();
+        // $scope.errorText = 'Error registering user';
+      // }
+      // else {
+        // $location.path('/offer-list');
+      // }
+    // }
+    // $scope.$apply();
   };
 });
 
