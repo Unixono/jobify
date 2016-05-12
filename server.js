e92309289e25d4bdf7b17920d1d03de75c09eba0
carneroalejandro <carneroalejandro@gmail.com>
2016-05-12 18:41:29 -0300

    Fix cors origin

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ app.use(passport.session());
 
 // Add this to allow CORS.
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://172.17.8.101:8080');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', false);
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
