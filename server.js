cbb1f44732b431de7f1494daa0573a2da7abd7eb
carneroalejandro <carneroalejandro@gmail.com>
2016-05-12 17:33:02 -0300

    Server acces fixed

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', false);
   next();
 });
 
