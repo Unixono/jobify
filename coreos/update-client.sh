58bca2e2f3d0e1dc657ea779779cfb1f075e0e48
carneroalejandro <carneroalejandro@gmail.com>
2016-05-31 16:16:51 -0300

    Create scripts to update the server & client code

---
+++ coreos/update-client.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+clear
+echo clear and fetch the repo
+git fetch -n -b release https://github.com/Unixono/jobify.git --depth 1
+echo .
+echo bring the client files up
+rm client -rf
+cd jobify
+git checkout HEAD public/dist
+mv public ../client
+echo .
+echo bring the coreos files up
+git checkout HEAD coreos/update-server.sh
+git checkout HEAD coreos/deploy.sh
+git checkout HEAD coreos/jobify-mongo.service
+git checkout HEAD coreos/jobify-server.service
+git checkout HEAD coreos/jobify-client.service
+mv coreos/* ../ -f
+rm coreos -rf
+cd ..
+rm jobify -rf
+echo .
+echo destroy coreos units
+fleetctl destroy jobify-client.service
+sleep 10
+echo .
+echo remove client image
+docker rmi jobify-client
+echo .
+echo build client image
+docker build -t jobify-client client
+echo .
+echo run coreos units
+fleetctl start jobify-client.service
+sleep 5
--- coreos/update-server.sh
+++ coreos/update-server.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 clear
 echo clear and fetch the repo
-git clone -n -b release https://github.com/Unixono/jobify.git --depth 1
+git fetch -n -b release https://github.com/Unixono/jobify.git --depth 1
 echo .
 echo bring the server files up
+rm server -rf
 mv jobify server
 cd server
 git checkout HEAD Dockerfile
@@ -14,32 +15,26 @@ git checkout HEAD LICENSE
 git checkout HEAD package.json
 git checkout HEAD README.md
 echo .
-echo bring the client files up
-git checkout HEAD public/dist
-mv public ../client
-echo .
 echo bring the coreos files up
-git checkout HEAD coreos/update.sh
+git checkout HEAD coreos/update-client.sh
+git checkout HEAD coreos/deploy.sh
 git checkout HEAD coreos/jobify-mongo.service
 git checkout HEAD coreos/jobify-server.service
 git checkout HEAD coreos/jobify-client.service
-mv coreos/* ../
-rm coreos -r
+mv coreos/* ../ -f
+rm coreos -rf
 cd ..
 echo .
-echo build server image
-docker pull mongo
+echo destroy coreos units
+fleetctl destroy jobify-server.service
+sleep 10
+echo .
+echo remove server image
+docker rmi jobify-server
 echo .
 echo build server image
 docker build -t jobify-server server
 echo .
-echo build client image
-docker build -t jobify-client client
-echo .
 echo run coreos units
-fleetctl start jobify-mongo.service
-sleep 10
 fleetctl start jobify-server.service
-sleep 10
-fleetctl start jobify-client.service
 sleep 5
